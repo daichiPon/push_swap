340cc90a58e1c7999d12052b351ccad82f84fb66
hsachie <sachie.duck.1113@icloud.com>
2026-06-06 19:03:03 +0900

    操作関数完成

---
+++ srcs/operations/rotate.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsachie <hsachie@student.42.jp>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/06 18:40:25 by hsachie           #+#    #+#             */
+/*   Updated: 2026/06/06 18:45:08 by hsachie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void    rotate(t_stack *s)
+{
+	t_node  *tmp;
+    t_node  *last;
+
+    if (s->size == 0)
+        return;
+    tmp = s->top;
+    s->top = s->top->next;
+    s->top->prev = NULL;
+    last = s->top;
+    while (last->next)
+        last = last->next;
+    last->next = tmp;
+    tmp->prev = last;
+    tmp->next = NULL;
+}
